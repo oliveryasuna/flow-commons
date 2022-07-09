1bbc08b0195487a4ff1f03a388fe6b53b1ad0190
Oliver Yasuna <oliveryasuna.acct@gmail.com>
2022-07-09 14:58:46 -0400

    Created the Clipboard component.

---
+++ src/main/java/com/oliveryasuna/vaadin/commons/component/clipboard/Clipboard.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2022 Oliver Yasuna
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without
+ *      specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.oliveryasuna.vaadin.commons.component.clipboard;
+
+import com.oliveryasuna.commons.language.function.Action;
+import com.vaadin.flow.component.ClientCallable;
+import com.vaadin.flow.component.html.Span;
+
+import java.util.function.Consumer;
+
+/**
+ * A component for working with the clipboard.
+ *
+ * @author Oliver Yasuna
+ * @since 4.0.0
+ */
+public class Clipboard extends Span {
+
+  // Constructors
+  //--------------------------------------------------
+
+  public Clipboard(final Consumer<String> getCallback, final Action setCallback) {
+    super();
+
+    getStyle().set("display", "none");
+
+    this.getCallback = getCallback;
+    this.setCallback = setCallback;
+  }
+
+  // Fields
+  //--------------------------------------------------
+
+  protected final Consumer<String> getCallback;
+
+  protected final Action setCallback;
+
+  // Methods
+  //--------------------------------------------------
+
+  public void get() {
+    getUI().ifPresent(ui -> ui.getPage().executeJs("navigator.clipboard.readText().then(text => $0.$server.getCallback(text));", getElement()));
+  }
+
+  @ClientCallable
+  public void getCallback(final String text) {
+    if(getCallback != null) getCallback.accept(text);
+  }
+
+  public void set(final String text) {
+    getUI().ifPresent(ui -> ui.getPage().executeJs("navigator.clipboard.writeText($0).then(() => $0.$server.setCallback());", getElement()));
+  }
+
+  @ClientCallable
+  public void setCallback() {
+    if(setCallback != null) setCallback.perform();
+  }
+
+}
